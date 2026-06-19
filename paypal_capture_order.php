92a026c1bfc57bddc43e2a20cd2b62b9115e58f1
desplegarChido <hasweek1atm23@gmail.com>
2026-06-19 01:12:25 -0600

    .

--- paypal_capture_order.php
+++ paypal_capture_order.php
@@ -1,6 +1,14 @@
 <?php
 //backend/paypal_capture_orden.php
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Content-Type: application/json");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    http_response_code(200);
+    exit();
+}
 require_once "config/database.php";
 
 // Credenciales de PayPal Sandbox (Business)
--- paypal_create_order.php
+++ paypal_create_order.php
@@ -1,7 +1,15 @@
 <?php
 //backend/paypal_create_orden.php
 
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Content-Type: application/json");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    http_response_code(200);
+    exit();
+}
 
 
 // Credenciales de PayPal Sandbox (Business)
